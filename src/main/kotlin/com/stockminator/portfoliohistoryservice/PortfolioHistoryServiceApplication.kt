7cfd8fdaad280525cb9900191278142ef062bd39
attilapalfi92 <apalfi@cloudera.com>
2021-03-18 17:13:40 +0100

    first commit

---
+++ src/main/kotlin/com/stockminator/portfoliohistoryservice/PortfolioHistoryServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.stockminator.portfoliohistoryservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortfolioHistoryServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<PortfolioHistoryServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/stockminator/portfoliohistoryservice/PortfolioHistoryServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.stockminator.portfoliohistoryservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioHistoryServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
